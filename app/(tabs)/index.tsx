35ea97409f53bb4be00f9e9d15993fb9adcbfbe4
OProgramadorNato <rm93980@estudante.fieb.edu.br>
2026-03-07 08:47:19 -0300

    atualização básica, iniciação de limpeza de pastas

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,43 +1,53 @@
-import { Image, StyleSheet, Text, View } from 'react-native';
-import { typography } from '../globalstyles/fonts';
-import Logo1 from '../images/LogoCircle.png';
-
+import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
+import { typography } from "../globalstyles/fonts";
+import Logo1 from "../images/LogoCircle.png";
 
 export default function HomeScreen() {
   return (
-      <View style={styles.container}>
-  <View style={styles.boxtop}>
-    <Image source={Logo1} style={styles.logocircle} />
+    <View style={styles.container}>
+      <View style={styles.boxtop}>
+        <Image source={Logo1} style={styles.logocircle} />
 
-    <Text style={typography.title}>
-      Se prepare para uma nova <Text style={{ color: '#F05221' }}>experiência</Text>
-    </Text>
+        <Text style={typography.title}>
+          Se prepare para uma nova{" "}
+          <Text style={{ color: "#F05221" }}>experiência</Text>
+        </Text>
 
-    <View style={styles.boxmid}>
-      <Text style={[typography.title, { color: '#8D8D8D' }]}>
-        Você está a poucos passos de criar o seu perfil e apresentar ao mundo!
-      </Text>
+        <View style={styles.boxmid}>
+          <Text
+            style={[
+              typography.title,
+              { color: "#8D8D8D", textAlign: "center" },
+            ]}
+          >
+            Você está a poucos passos de criar o seu perfil e apresentar ao
+            mundo!
+          </Text>
+        </View>
+      </View>
     </View>
-
-  </View>
-</View>
   );
 }
-
+8;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-
+    justifyContent: "center",
+    alignItems: "center",
   },
   boxtop: {
-    alignItems: 'center'
+    alignItems: "center",
+    backgroundColor: "red",
+    width: "100%",
+    height: Dimensions.get("window").height / 2,
   },
   boxmid: {
-    textAlign: 'center',
-    alignItems: 'center',
-    justifyContent: 'center',
+    textAlign: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "blue",
+    width: "100%",
+    height: Dimensions.get("window").height / 3,
   },
   logocircle: {
     height: 175,
